a297a856a3fc8234f79856abbc787c4b4bf82eb9
Asif Azimi <iasifazimi@outlook.com>
2022-09-12 16:00:12 +0430

    Add Buisness and Stats

--- src/components/Buisness.jsx
+++ src/components/Buisness.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { icon } from "../assets";
+
+const FeatureCard = ({ title, icon, content, index }) => {
+  return (
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index === features.length - 1 ? "mb-0" : "mb-6"
+      } feature-card`}
+    >
+      <div
+        className={`w-[58px] h-[58px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      >
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+          {title}
+        </h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const Buisness = () => {
-  return <div>Buisness</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the buisness, <br className="sm:block hidden" />
+          we'll handle the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => {
+          return <FeatureCard key={feature.id} {...feature} index={index} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Buisness;
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary outline-none text-[18px] rounded-md  ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
