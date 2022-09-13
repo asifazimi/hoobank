9a6e92b8960217f26ba37f8ae7ce7aef6efa41e7
Asif Azimi <iasifazimi@outlook.com>
2022-09-13 17:35:12 +0430

    Add CardDeal and Billing sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary outline-none text-[18px] rounded-md  ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary outline-none text-[18px] rounded-[10px]  ${styles}`}
     >
       Get Started
     </button>
