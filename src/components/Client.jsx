180831ac4f81ecec915dd80134d5ac120d35c7a7
Asif Azimi <iasifazimi@outlook.com>
2022-09-14 17:31:49 +0430

    Add CTA and Client sections

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
 const Client = () => {
-  return <div>Client</div>;
+  return (
+    <section className={`${styles.flexCenter} my-10`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => {
+          const { logo, id } = client;
+          return (
+            <div
+              key={id}
+              className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+            >
+              <img
+                src={logo}
+                alt="logo"
+                className="sm:w-[192px] w-[100px] object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Client;
